1de2ee9b4ba3c528008936d204c0f6eacca6d618
fabiokaspar <fabiokaspar@gmail.com>
2025-04-16 18:56:23 -0300

    remove uvicorn command

--- main.py
+++ main.py
@@ -33,12 +33,3 @@ async def health_check():
 
 app.include_router(cliente_router)
 app.include_router(produto_router)
-
-if __name__ == "__main__":
-    print("Starting server...")
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
